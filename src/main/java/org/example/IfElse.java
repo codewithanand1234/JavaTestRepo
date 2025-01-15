d380eee11a52b98d7860c5f9d856a7925776bb95
Anand <anandsrivastav766@gmail.com>
2025-01-15 20:40:55 +0530

    If else Added

---
+++ src/main/java/org/example/IfElse.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class IfElse {
+    public static void main(String[] args) {
+        System.out.println("This is if else code file");
+        int num=12;
+        if(num%2==0)
+        {
+            System.out.println("Even number");
+        }
+        else
+        {
+            System.out.println("Odd Number");
+        }
+    }
+}
